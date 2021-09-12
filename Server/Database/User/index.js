cdb5b474c169c003e21c41379f6cbb1fb37b8d0d
“your <https://github.com/DhruvMakadia17>
2021-09-12 16:27:33 +0530

    Improved Signup -> Statics And Methods

--- Server/Database/User/index.js
+++ Server/Database/User/index.js
@@ -1,16 +1,55 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-const UserSchema = new mongoose.Schema({
-    fullname : {type: String, required: true},
-    email : {type: String, required: true},
+const UserSchema = new mongoose.Schema(
+    {
+    fullname : { type: String, required: true },
+    useremail : { type: String, required: true },
     password : { type: String },
     address: [{ detail: {type: String}, for: { type:String } }],
     phoneNumber: [{ type: Number }],
 
-},
-{
-    timestamps: true,
-}
+    },
+    {
+        timestamps: true,
+    }
 );
+
+UserSchema.methods.generateJwtToken = function () {
+    return jwt.sign({user: this._id.toString()}, "ZomatoAPP");
+};
+//Statics And Methods
+
+UserSchema.statics.findByEmailAndPhone = async ({ useremail, phoneNumber }) => {
+    // Check whether email exists
+    const checkUserByEmail = await UserModel.findOne({ useremail });
+    const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+    if(checkUserByEmail || checkUserByPhone){
+        throw new Error("User Already Exist!");
+    }
+    return false;
+};
+
+UserSchema.pre("save", function (next){
+    const user = this;
+    
+    //password is mpodified
+    if(!user.isModified("password")) return next();
+    
+    //generate bcrypt salt
+    bcrypt.genSsalt(8, (error, salt) => {
+        if(error) return next(error);
+
+        //hash the password
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if(error) return next(error);
+            
+            // assigned hashed password
+            user.password = hash;
+            return next(); 
+        });
+    });
+});
 
 export const UserModel = mongoose.model("Users", UserSchema);
