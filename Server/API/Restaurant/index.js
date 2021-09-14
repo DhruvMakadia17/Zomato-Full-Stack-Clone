366451cc29e9f5043958f3767bfee15c3c9b161c
“your <https://github.com/DhruvMakadia17>
2021-09-14 22:50:32 +0530

    Added restautant validation

--- Server/API/Restaurant/index.js
+++ Server/API/Restaurant/index.js
@@ -5,6 +5,10 @@ import passport from "passport";
 //Database Model
 import { RestaurantModel } from "../../Database/allModels";
 
+//validation
+import { ValidateRestaurantCity, ValidateRestaurantSearchString } from "../../Validation/restaurant";
+import { ValidateResaturantId, ValidateRestaurantId } from "../../Validation/food";
+
 const Router = express.Router();
 /*
 Route   /
@@ -15,6 +19,9 @@ Method  GET
 */
 Router.get("/", async(req, res) => {
     try{
+        //Validation
+        await ValidateRestaurantCity(req.query);
+
         const { city } = req.query;
         const restaurants = await RestaurantModel.find({ city });
 
@@ -34,6 +41,9 @@ Method  GET
 */
 Router.get("/:_id", async (req, res) => {
     try {
+        //Validation
+        await ValidateRestaurantId(req.params);
+
         const { id } = req.params;
         const restaurant = await RestaurantModel.findOne(_id);
         if(!restaurant) return res.status(404).json({ error: "Restaurant Not Found" });
@@ -54,6 +64,9 @@ Method  GET
 */
 Router.get("/search", async (req, res) => {
     try {
+        //
+        await ValidateRestaurantSearchString(req.body);
+
         const { searchString } = req.body;
         const restaurants = await RestaurantModel.find({ 
             name: { $regex: searchString, $options: "i" },
